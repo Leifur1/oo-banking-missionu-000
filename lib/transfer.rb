a7ced7c270ce303427ca1191dc82264dccf97843
Leifur Sigurdsson <leifurerik@gmail.com>
2018-10-28 01:03:57 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,11 @@
 class Transfer
-  # your code here
+  attr_reader :sender, :receiver, :transfer
+  attr_accessor :status
+
+  def initialize(sender, receiver, transfer)
+    @sender = sender
+    @receiver = receiver
+    @status = "pending"
+    @transfer = transfer
+  end
 end
